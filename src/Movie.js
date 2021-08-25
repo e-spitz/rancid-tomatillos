9dc25eba2bbec42c316c47f3c7ae1abc4810201e
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-08-25 17:31:05 -0500

    Add jsx structure to Movie component

--- src/Movie.js
+++ src/Movie.js
@@ -1,2 +1,15 @@
 import React, { Component } from 'react';
 import './Movie.css'
+
+const Movie = (props) => {
+    return  (
+        <div className="movie-card">
+            <img src={props.poster_path}></img>
+            <h2>{props.title}</h2>
+            <p>{props.average_rating.toFixed(1)}</p>
+            <p>{props.release_date}</p>
+        </div>
+    )
+}
+
+export default Movie;
