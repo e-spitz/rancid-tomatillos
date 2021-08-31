88c77eaf132d9f36c895844131af66a1b6441a60
Erica <ericaspitz522@gmail.com>
2021-08-30 19:09:16 -0600

    Add opacity and styling to detail modal

--- src/Components/MovieDetails/MovieDetails.js
+++ src/Components/MovieDetails/MovieDetails.js
@@ -9,22 +9,22 @@ const MovieDetails = ({ movie, goToMainView, trailer }) => {
     console.log(videoLink)
     return  (
       <section className='movie-details' style={{ backgroundImage: `url(${movie.backdrop_path})` }}>
-      <button onClick={() => goToMainView()}>&times;</button>
         <article className='movie-details-info'>
             <h2>{movie.title}</h2>
             <p>Rating: {movie.average_rating.toFixed(1)}</p>
             <p>Release Date: {newDate}</p>
             <div className='trailer-video'>
-            <iframe
-            title='Embedded youtube'
-            className='video'
-            width='250'
-            height='200'
-            src={videoLink}
-            frameBorder='0'
-            allowFullScreen
-            ></iframe>
+              <iframe
+              title='Embedded youtube'
+              className='video'
+              width='250'
+              height='200'
+              src={videoLink}
+              frameBorder='0'
+              allowFullScreen
+              ></iframe>
             </div>
+            <button onClick={() => goToMainView()}>&times;</button>
         </article>
       </section>
     )
