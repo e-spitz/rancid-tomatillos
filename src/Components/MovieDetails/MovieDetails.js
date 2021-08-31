d87294393ca8d85f825d1f28213f0d667d8417c5
Erica <ericaspitz522@gmail.com>
2021-08-31 08:57:50 -0600

    Add ternary operators in movie details

--- src/Components/MovieDetails/MovieDetails.js
+++ src/Components/MovieDetails/MovieDetails.js
@@ -6,17 +6,19 @@ const MovieDetails = ({ movie, goToMainView, trailer }) => {
     const splitDate = movie.release_date.split('-')
     const newDate = splitDate[1] + '-' + splitDate[2] + '-' + splitDate[0]
     const videoLink = `https://www.youtube.com/embed/${trailer.key}`
+    const allGenres = movie.genres.map(genre => genre + ' ')
 
     return  (
       <section className='movie-details' style={{ backgroundImage: `url(${movie.backdrop_path})` }}>
         <article className='movie-details-info'>
             <h2>{movie.title}</h2>
-            <h3>"{movie.tagline}"</h3>
+            <h3>{movie.tagline}</h3>
+            <h4 style={{display: !movie.overiew && "none"}}>{movie.overview}</h4>
             <p>Avg Rating: {movie.average_rating.toFixed(1)}</p>
             <p>Release Date: {newDate}</p>
-            <p>Genre: {movie.genres}</p>
-            <p>Budget: {movie.budget}</p>
-            <p>Revenue: {movie.revenue}</p>
+            <p>Genre: {allGenres}</p>
+            <p>Budget: {!movie.budget ? "Unavailable" : movie.budget}</p>
+            <p>Revenue: {!movie.revenue ? "Unavailable" : movie.revenue}</p>
             <p>Runtime: {movie.runtime} minutes</p>
             <div className='trailer-video'>
               <iframe
@@ -34,6 +36,7 @@ const MovieDetails = ({ movie, goToMainView, trailer }) => {
       </section>
     )
 }
+
 
 MovieDetails.propTypes = {
     movie: PropTypes.object.isRequired,
