7359510d9629fc0483b97a47fde6a4d8cb5eb4db
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-09-01 14:55:07 -0500

    Refactor MovieDetails component to class-based-component

--- src/Components/MovieDetails/MovieDetails.js
+++ src/Components/MovieDetails/MovieDetails.js
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import apiCalls from '../../apiCalls'
 import './MovieDetails.css'
+const url = 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/'
 
-const MovieDetails = ({ movie, goToMainView, trailer }) => {
-    const splitDate = movie.release_date.split('-')
-    const newDate = splitDate[1] + '-' + splitDate[2] + '-' + splitDate[0]
-    const videoLink = `https://www.youtube.com/embed/${trailer.key}`
+// const MovieDetails = ({ movie, goToMainView, trailer }) => {
+    class MovieDetails extends Component {
 
-    return  (
-      <section className='movie-details' style={{ backgroundImage: `url(${movie.backdrop_path})` }}>
+    // const splitDate = movie.release_date.split('-')
+    // const newDate = splitDate[1] + '-' + splitDate[2] + '-' + splitDate[0]
+    // const videoLink = `https://www.youtube.com/embed/${trailer.key}`
+
+    constructor(props) {
+      super(props);
+      this.state = {
+          movieInfo: {},
+          movieTrailers: '',
+          movieError: '',
+          videoError: '',
+      }
+  }
+
+  componentDidMount() {
+    apiCalls.getMovieData(url + this.props.id)
+        .then(movie => this.setState({ movieInfo: movie.movie }))
+        .catch(() => this.setState({ movieError: 'Having trouble finding movie information right now...please try again.'} ));
+
+    // fetchVideo(this.props.id)
+    //     .then(video => {this.setState({movieTrailers: video.videos})})
+    //     .catch(() => this.setState({ videoError: 'Sorry, this video is currently unavailable.'}))
+  }
+
+
+
+    render() {
+      console.log(this.state.movieInfo)
+      return (
+      // <section className='movie-details' style={{ backgroundImage: `url(${movie.backdrop_path})` }}>
         <article className='movie-details-info'>
-            <h2>{movie.title}</h2>
-            <h3>"{movie.tagline}"</h3>
+            <h2>TEST</h2>
+            {/* <h3>"{movie.tagline}"</h3>
             <p>Avg Rating: {movie.average_rating.toFixed(1)}</p>
             <p>Release Date: {newDate}</p>
             <p>Genre: {movie.genres}</p>
@@ -30,11 +59,11 @@ const MovieDetails = ({ movie, goToMainView, trailer }) => {
               allowFullScreen
               ></iframe>
             </div>
-            <Link to="/">Main View</Link>
-            {/* <button onClick={() => goToMainView()}>&times;</button> */}
+            <Link to="/">Main View</Link> */}
         </article>
-      </section>
-    )
+      // </section>
+      )
+    }
 }
 
 // MovieDetails.propTypes = {
