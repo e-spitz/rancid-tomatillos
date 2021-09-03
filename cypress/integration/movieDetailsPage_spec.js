2894d132de75dd4ff18d96a781bbfa7902d78039
Erica <ericaspitz522@gmail.com>
2021-09-03 11:37:58 -0600

    Add go back and forward tests

--- cypress/integration/movieDetailsPage_spec.js
+++ cypress/integration/movieDetailsPage_spec.js
@@ -19,12 +19,6 @@ describe('Movie Details page', () => {
         .should('include', 'https://www.youtube.com/embed/KK8FHdFluOQ')
   });
 
-  it('Should return to main page when clicking Main View', () => {
-    cy.get('.go-back')
-      .click()
-    cy.url().should('eq','http://localhost:3000/')
-  });
-
   it('Should be rendering Movie data on a single movie page', () => {
     cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/337401', {
       statusCode: 201
@@ -32,7 +26,19 @@ describe('Movie Details page', () => {
     cy.get('.movie-details').should('have.attr', 'style').should('include', 'https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg')
   });
 
+  it('Should return to main page when clicking Main View', () => {
+    cy.get('.go-back')
+      .click()
+    cy.url().should('eq','http://localhost:3000/')
+  });
 
+  it('Should be able to use the back and forward buttons within the browser', () => {
+    cy.visit('http://localhost:3000/')
+    cy.go('back')
+    cy.url().should('eq','http://localhost:3000/337401')
+    cy.go('forward')
+    cy.url().should('eq','http://localhost:3000/')
+  });
 
 
 });
