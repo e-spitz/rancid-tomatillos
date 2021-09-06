fded9182212b23054a1a8c94a1c5857e0e975fb2
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-09-06 11:16:50 -0500

    Styling input filds

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './Error.css'
-// import PageNotFound from '../assets/images/PageNotFound';
 
 const Error = () => {
     return (
         <div className='error'>
             <NavLink className='go-back-main' to='/'>
-                <img src="https://files.muzli.space/43e6e439756832db0ff5dd2b76ffef5c.jpeg" />
+                <img src="https://files.muzli.space/43e6e439756832db0ff5dd2b76ffef5c.jpeg" alt="404-img"/>
             </NavLink>
         </div>
     )
